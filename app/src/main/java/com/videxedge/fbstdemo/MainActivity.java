6c55de0682cb0d339978f87286d29bd16b5f22c4
Albert Levy <albert.school2015@gmail.com>
2019-12-04 20:40:33 +0200

    End of design

--- app/src/main/java/com/videxedge/fbstdemo/MainActivity.java
+++ app/src/main/java/com/videxedge/fbstdemo/MainActivity.java
@@ -3,12 +3,37 @@ package com.videxedge.fbstdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * @author		Albert Levy <albert.school2015@gmail.com>
+ * @version     1.0
+ * @since		4/12/2019
+ * Basic application to demonstrate upload & download file to Firebase Storage
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Uploading selected image file to Firebase Storage
+     * <p>
+     *
+     * @param view
+     */
+    public void upload(View view) {
+    }
+
+    /**
+     * Downloading selected image file to Firebase Storage
+     * <p>
+     *
+     * @param view
+     */
+    public void download(View view) {
     }
 }
